31bd7254ed54c2ce16e87c74a33e29cff72b7833
shinpei <shinpei.nakata@gmail.com>
2015-08-03 14:53:53 +0900

    replace now works

--- src/main/java/com/github/shinpei/jmxcli/Printer.java
+++ src/main/java/com/github/shinpei/jmxcli/Printer.java
@@ -1,19 +1,21 @@
 package com.github.shinpei.jmxcli;
 
+import com.google.common.base.CharMatcher;
+
 import java.io.PrintWriter;
 
 public class Printer {
     static void P(String fmt, int arg) {
-        fmt.replace("{}", "%d");
+        fmt = fmt.replace("{}", "%d");
         System.out.printf(fmt + "\n", arg);
     }
     static void P(String fmt, String arg) {
-        fmt.replace("{}", "%s");
+        fmt = fmt.replace("{}", "%s");
         System.out.printf(fmt + "\n", arg);
     }
 
     static void P(String fmt, Object arg) {
-        fmt.replace("{}", "%s");
+        fmt = fmt.replace("{}", "%s");
         System.out.printf(fmt + "\n", arg);
     }
 
